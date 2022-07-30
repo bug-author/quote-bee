249e19ba4f755e9fc48355534651a7bc21fb88b3
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-07-30 23:31:23 +0500

    add home ui

---
+++ app/quote_bee/lib/pages/home_page.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:quote_bee/constants.dart';
+import 'package:quote_bee/pages/quote_card.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> navbarItems = [
+    "All",
+    "Most Liked",
+    "Most Retweeted",
+    "By Year",
+    "Favourites"
+  ];
+
+  int bottomBarIdx = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // https://stackoverflow.com/questions/51686868/gradient-text-in-flutter
+    final Shader linearGradient = LinearGradient(
+      colors: <Color>[Color(0xFFEA8CCD), Color(0xFF6573A9)],
+    ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
+
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Quote Bee',
+            style: TextStyle(
+                fontSize: 35,
+                fontFamily: 'LibreFranklin',
+                fontWeight: FontWeight.bold,
+                foreground: Paint()..shader = linearGradient),
+          ),
+          backgroundColor: darkBlue,
+          elevation: 0.0,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.nightlight_round, size: 21),
+              onPressed: () {},
+            ),
+          ]),
+      body: Container(
+        color: darkBlue,
+        height: size.height,
+        child: Column(
+          children: [
+            Container(
+              height: size.height * 0.15,
+              width: size.width,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: navbarItems.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: (){
+                      print(navbarItems[index]);
+                    },
+                    child: Container(
+                        height: size.height * 0.05,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: size.width * 0.05,
+                            vertical: size.height * 0.05),
+                        margin:
+                            EdgeInsets.symmetric(horizontal: size.width * 0.03),
+                        // decoration: BoxDecoration(color: darkBlue,),
+                        child: Text(
+                          navbarItems[index],
+                          style: const TextStyle(
+                            fontSize: 22,
+                            color: grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.6,
+              child: Stack(
+                children: [
+                  quote_card(size),
+                  quote_card(size),
+                  quote_card(size),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: darkerBlue,
+          selectedItemColor: Colors.white,
+          // https://stackoverflow.com/questions/52182384/flutter-how-to-hide-remove-title-of-bottomnavigationbaritem
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: bottomBarIdx,
+          onTap: (index) => setState(() => bottomBarIdx = index),
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 35,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                size: 35,
+              ),
+              label: 'Search',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
