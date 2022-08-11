72d3d3ee8fd20e38e2351ef63d9652582418bb44
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-08-11 23:50:27 +0500

    refactor

--- app/quote_bee/lib/provider/quote_provider.dart
+++ app/quote_bee/lib/provider/quote_provider.dart
@@ -1,65 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:quote_bee/db_helper/db_helper.dart';
 import '../model/quote_model.dart';
 
 class QuoteProvider extends ChangeNotifier {
   List<QuoteModel> quoteItems = [];
+  Color borderColor = Colors.red;
+  Color iconColor = Colors.red;
+
+  List<QuoteModel> mapDataListToModel(List<Map> dataList) {
+    return dataList
+        .map(
+          (item) => QuoteModel(
+            index: item['index'],
+            date: item['Date'],
+            tweet: item['Tweet'],
+            url: item['URL'],
+            likeCount: item['Like Count'],
+            retweetCount: item['Retweet Count'],
+            favourite: item['Favourite'],
+          ),
+        )
+        .toList();
+  }
 
   // call and implement sql select function
   Future<void> selectData(int index) async {
     if (index == 0) {
       final dataList = await DBHelper.selectRandom();
-
-      quoteItems = dataList
-          .map((item) => QuoteModel(
-              index: item['index'],
-              date: item['Date'],
-              tweet: item['Tweet'],
-              url: item['URL'],
-              likeCount: item['Like Count'],
-              retweetCount: item['Retweet Count'],
-              favourite: item['Favourite']))
-          .toList();
+      quoteItems = mapDataListToModel(dataList);
     } else if (index == 1) {
       final dataList = await DBHelper.selectMostLiked();
-
-      quoteItems = dataList
-          .map((item) => QuoteModel(
-              index: item['index'],
-              date: item['Date'],
-              tweet: item['Tweet'],
-              url: item['URL'],
-              likeCount: item['Like Count'],
-              retweetCount: item['Retweet Count'],
-              favourite: item['Favourite']))
-          .toList();
+      quoteItems = mapDataListToModel(dataList);
     } else if (index == 2) {
       final dataList = await DBHelper.selectMostRetweeted();
-
-      quoteItems = dataList
-          .map((item) => QuoteModel(
-              index: item['index'],
-              date: item['Date'],
-              tweet: item['Tweet'],
-              url: item['URL'],
-              likeCount: item['Like Count'],
-              retweetCount: item['Retweet Count'],
-              favourite: item['Favourite']))
-          .toList();
+      quoteItems = mapDataListToModel(dataList);
     } else {
       final dataList = await DBHelper.selectFavourites();
-
-      quoteItems = dataList
-          .map((item) => QuoteModel(
-              index: item['index'],
-              date: item['Date'],
-              tweet: item['Tweet'],
-              url: item['URL'],
-              likeCount: item['Like Count'],
-              retweetCount: item['Retweet Count'],
-              favourite: item['Favourite']))
-          .toList();
+      quoteItems = mapDataListToModel(dataList);
     }
 
     notifyListeners();
@@ -67,6 +44,7 @@ class QuoteProvider extends ChangeNotifier {
 
   Future insertFav(String url, int oldfavStatus) async {
     await DBHelper.insertFav(url, oldfavStatus);
+
     notifyListeners();
   }
 }
