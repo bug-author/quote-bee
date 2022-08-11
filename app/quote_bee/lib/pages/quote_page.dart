51c88117a298bc09097d799bb2547da8cd76b302
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-08-11 23:50:50 +0500

    refactor

--- app/quote_bee/lib/pages/quote_page.dart
+++ app/quote_bee/lib/pages/quote_page.dart
@@ -4,6 +4,7 @@ import 'package:quote_bee/pages/quote.dart';
 import 'package:quote_bee/provider/quote_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:quote_bee/widget/change_theme_widget.dart';
+import 'package:quote_bee/widget/horizontal_categories_widget.dart';
 
 import '../provider/theme_provider.dart';
 
@@ -42,53 +43,8 @@ class QuotePage extends StatelessWidget {
               return quoteProvider.quoteItems.isNotEmpty
                   ? Column(
                       children: [
-                        SizedBox(
-                          height: height * 0.15,
-                          width: width,
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            scrollDirection: Axis.horizontal,
-                            itemCount: navbarItems.length,
-                            itemBuilder: (context, index) {
-                              return GestureDetector(
-                                onTap: () {
-                                  if (index != category) {
-                                    Navigator.of(context).pushAndRemoveUntil(
-                                        MaterialPageRoute(
-                                            builder: (context) => QuotePage(
-                                                  category: index,
-                                                )),
-                                        (Route<dynamic> route) => false);
-                                  }
-                                },
-                                child: Container(
-                                  height: height * 0.05,
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: width * 0.05,
-                                    vertical: height * 0.05,
-                                  ),
-                                  margin: EdgeInsets.symmetric(
-                                    horizontal: width * 0.03,
-                                  ),
-                                  child: Text(
-                                    navbarItems[index],
-                                    style: TextStyle(
-                                      fontSize: 22,
-                                      color: index == category
-                                          ? Provider.of<ThemeProvider>(context)
-                                                      .themeMode ==
-                                                  ThemeMode.dark
-                                              ? Colors.white
-                                              : Colors.black87
-                                          : grey,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
+                        HorizontalCategories(
+                            height: height, width: width, category: category),
                         Expanded(
                           child: SizedBox(
                               height: height * 0.75,
@@ -101,13 +57,20 @@ class QuotePage extends StatelessWidget {
                         ),
                       ],
                     )
-                  : const Center(
-                      child: Text(
-                      "No tweets in database",
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    ));
+                  : Column(
+                      children: [
+                        HorizontalCategories(
+                            height: height, width: width, category: category),
+                        const Spacer(),
+                        const Text(
+                          "No tweets in database",
+                          style: TextStyle(
+                            fontSize: 30,
+                          ),
+                        ),
+                        const Spacer()
+                      ],
+                    );
             });
           } else {
             return const Center(
